2db4460634cfd30b6ed7c326d0d21a2cc14135a1
Volodymyr Polishchuk <v.polishchuk@pollabs.io>
2023-04-28 12:43:04 +0300

    Update images, token and url for widget

--- src/screens/WidgetScreen/WidgetScreen.tsx
+++ src/screens/WidgetScreen/WidgetScreen.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import TabScreenSafeAreaWrapper from '../../components/TabScreenSafeAreaWrapper';
 import WebView from 'react-native-webview';
 
+const TOKEN =
+  'eyJhbGciOiJQUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiVVNFUl9BQ0NFU1MiLCJpYXQiOjE2ODI2NzEyMDEsIm5iZiI6MTY4MjY3MTIwMSwiaXNzIjoibGYiLCJzdWIiOiIxIiwiZXhwIjoxNjgyNzU3NjAxLCJhdWQiOltdfQ.u6Cn6-agPY-dxezBFs4CIElKLGb6Q615rmtIfazTglPbRj4KiM-3GgFaN7L5Ou5G2OnE2J8tGTVssitJGDqioBY_DDeQaeFsUsI2yID4znLaAKimlWcs_IfyYrODCxEpeCpLTRieIj55aiX_DUSKD-jy7l9xT8qYYXwE5o8HMCBjORGHT68ECe28fe_mSqLWDY5sbC6hZYBmb2Jr-4g3asQH1kWLRcAtCQYTm-tpi-P0_oMyv5B738KjLgT3Xo0gr_lsOkpiJyCNy2-2vT8pL_g84ZvTdtxQ5wtjeFvMWpk-4hzY2FBm0r9DLYdh1gKZz4KZfan_jBqFsR9FeVs2wQ';
+
 const WidgetScreen = () => {
   return (
     <TabScreenSafeAreaWrapper>
       <WebView
         source={{
-          uri: 'https://coinflip.apps.scrimmage.co/widget/?token=eyJhbGciOiJQUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiVVNFUl9BQ0NFU1MiLCJpYXQiOjE2ODI2MTY0NjEsIm5iZiI6MTY4MjYxNjQ2MSwiaXNzIjoibGYiLCJzdWIiOiIxIiwiZXhwIjoxNjgyNzAyODYxLCJhdWQiOltdfQ.cGPKMglIITJhMtE4wrrxuPkLxkxbqKC82MRB7gjj-VMy_sluFmXJl8fRo-GeKr2t3CyA0qz_RzZ7sKshrtT5azOFq7MXrvISNJz6HSzywpMCHPz8KwC1385Lhg3_DRVzJ95uFycL2ZVq6xd7ODYJYh6AJAsXTenDfx087f6nJ6LB9ifgCBmbbKMb2oydqRx5dTH01YnzkVUaJ5WWvc_JRCTJESi9En7yVHKfqXxssvkrA4ZRKjzlRNd7szQpqGYPu8O_ITgZodzA7YLUwSznZGdFQWRgsQDGtMtWduW2nW6XDYTMGJ-IcAmOsqbFHrJHBROcIzL18-auDkmp-i04Ew',
+          uri: `https://coinflip.apps.scrimmage.co/?token=${TOKEN}`,
         }}
       />
     </TabScreenSafeAreaWrapper>
